02eceedb44005068448ee9774e329be65d5bc2db
ElmaLisboa <ikiushunik@gmail.com>
2025-12-25 21:13:10 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,20 @@ const app = express();
 
 app.use(express.json());
 
+// liberar acesso (CORS)
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
+// rota de teste
+app.get("/", (req, res) => {
+  res.send("Cérebro online 🚀");
+});
+
+// rota principal
 app.post("/gerar", (req, res) => {
   const urlRecebida = req.body.url;
 
